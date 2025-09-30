a6204c068de6f80834a868c0681b2683ab4c3613
shirnim <nimish.shirodkar@gmail.com>
2025-09-30 23:15:21 +0530

    Commiting changes to password encrypting

--- backend/security.py
+++ backend/security.py
@@ -31,13 +31,13 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # --- Password Hashing ---
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password[:72], hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password[:72])
+    return pwd_context.hash(password)
 
 # --- JWT Token ---
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
